7ce3e654f13a299c12b41f73b06b3b76cb486968
neel_patel <pneel141@gmail.com>
2023-06-08 01:39:17 +0530

    Configured TestNG file in POM file for ci-cd pipeline for jenkins

--- src/test/java/TestPractice2.java
+++ src/test/java/TestPractice2.java
@@ -4,12 +4,8 @@ import org.testng.annotations.Test;
 
 public class TestPractice2 {
 
-    @BeforeTest
-    public void beforeTest() {
-        System.out.println("TestPractice2:: Before Test ::");
-    }
 
-    @Test(enabled = false)
+    @Test(groups = {"sanity"})
     public void firstTest() {
         System.out.println("TestPractice2:: This is first test");
     }
@@ -24,9 +20,5 @@ public class TestPractice2 {
         System.out.println("TestPractice2:: This is third test");
     }
 
-    @AfterTest
-    public void afterTest() {
-        System.out.println("TestPractice2:: After Test ::");
-    }
 
 }
